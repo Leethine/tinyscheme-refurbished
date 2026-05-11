eff7e496c8895e1596b87cc79287ff11bb1d71b7
Z L <leethine@github.com>
2026-05-11 18:38:52 -0500

    Add test script

--- new_impl/bignum/factorial.scm
+++ new_impl/bignum/factorial.scm
@@ -1,6 +1,18 @@
+;; This doesn't work after some x
+(define (fact x)
+  (if 
+    (= x 0) 1
+    (* x (fact (- x 1)))
+  )
+)
+
+;; This supports big numbers
 (define (! x)
   (if 
     (= x 0) "1"
     (big-mul (number->string x) (! (- x 1)))
   )
 )
+
+(display (! 100))
+(newline)
